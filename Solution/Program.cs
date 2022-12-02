eea9acb442aa35efca275fceed9ec92eed3104c9
MrTihon <tihonsomov@mail.ru>
2022-12-02 13:59:46 +0300

    add Solution

---
+++ Solution/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] array = Input();
+string[] result = LessOreQually(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] LessOreQually(string[]
+enter, int n)
+{
+    string[] result = new string[CountLessOreQually(enter, n)];
+
+    for (int i = 0, j = 0; i < enter.Length; i++)
+    {
+        if (enter[i].Length <= n)
+        {
+            result[j] = enter[i];
+            j++;
+        }
+    }
+
+    return result;
+}
+
+int CountLessOreQually(string[] enter, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < enter.Length; i++)
+    {
+        if (enter[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] Input()
+{
+    Console.WriteLine("Введите значения через пробел: ");
+    string[] input = Console.ReadLine().Split();
+    return input;
+}
